3bcdb88fe99dec1fa15bbd7e86cde012f339467c
Edson Y. Toma <edson.toma@usp.br>
2018-11-05 14:31:56 -0200

    test predict

--- knn.py
+++ knn.py
@@ -49,76 +49,88 @@ ap.add_argument("-j", "--jobs", type=int, default=-1,
                 help="# of jobs for k-NN distance (-1 uses all available cores)")
 args = vars(ap.parse_args())
 
-# grab the list of images that we'll be describing
-print("[INFO] describing images...")
-imagePaths = list(paths.list_images(args["dataset"]))
 
-# initialize the raw pixel intensities matrix, the features matrix,
-# and labels list
-rawImages = []
-features = []
-labels = []
+try:
+	model = cPickle.loads(open("model.cpickle", "rb").read())  # to read model
+	readModel = 1
+except:
+	readModel = 0
 
-# loop over the input images
-for (i, imagePath) in enumerate(imagePaths):
-	# load the image and extract the class label (assuming that our
-	# path as the format: /path/to/dataset/{class}.{image_num}.jpg
-	image = cv2.imread(imagePath)
-	label = re.split('(\d+)', imagePath)[0]
-
-	# extract raw pixel intensity "features", followed by a color
-	# histogram to characterize the color distribution of the pixels
-	# in the image
+if readModel:
+	imagePaths = list(paths.list_images(args["dataset"]))
+	image = cv2.imread(imagePaths[0])
 	pixels = image_to_feature_vector(image)
-	hist = extract_color_histogram(image)
+	print(model.predict(pixels.reshape(1,-1)))
+else:
+	# grab the list of images that we'll be describing
+	print("[INFO] describing images...")
+	imagePaths = list(paths.list_images(args["dataset"]))
 
-	# update the raw images, features, and labels matricies,
-	# respectively
-	rawImages.append(pixels)
-	features.append(hist)
-	labels.append(label)
+	# initialize the raw pixel intensities matrix, the features matrix,
+	# and labels list
+	rawImages = []
+	features = []
+	labels = []
 
-	# show an update every 1,000 images
-	if i > 0 and i % 1000 == 0:
-		print("[INFO] processed {}/{}".format(i, len(imagePaths)))
+	# loop over the input images
+	for (i, imagePath) in enumerate(imagePaths):
+		# load the image and extract the class label (assuming that our
+		# path as the format: /path/to/dataset/{class}.{image_num}.jpg
+		image = cv2.imread(imagePath)
+		label = re.split('(\d+)', imagePath)[0]
 
-#print(labels)
+		# extract raw pixel intensity "features", followed by a color
+		# histogram to characterize the color distribution of the pixels
+		# in the image
+		pixels = image_to_feature_vector(image)
+		hist = extract_color_histogram(image)
 
-# show some information on the memory consumed by the raw images
-# matrix and features matrix
-rawImages = np.array(rawImages)
-features = np.array(features)
-labels = np.array(labels)
-print("[INFO] pixels matrix: {:.2f}MB".format(
-	rawImages.nbytes / (1024 * 1000.0)))
-print("[INFO] features matrix: {:.2f}MB".format(
-	features.nbytes / (1024 * 1000.0)))
+		# update the raw images, features, and labels matricies,
+		# respectively
+		rawImages.append(pixels)
+		features.append(hist)
+		labels.append(label)
 
-# partition the data into training and testing splits, using 75%
-# of the data for training and the remaining 25% for testing
-(trainRI, testRI, trainRL, testRL) = train_test_split(
-	rawImages, labels, test_size=0.25, random_state=42)
-(trainFeat, testFeat, trainLabels, testLabels) = train_test_split(
-	features, labels, test_size=0.25, random_state=42)
+		# show an update every 1,000 images
+		if i > 0 and i % 1000 == 0:
+			print("[INFO] processed {}/{}".format(i, len(imagePaths)))
 
-# train and evaluate a k-NN classifer on the raw pixel intensities
-print("[INFO] evaluating raw pixel accuracy...")
-model = KNeighborsClassifier(n_neighbors=args["neighbors"],
-                             n_jobs=args["jobs"], metric="cityblock") #manhattan distance
-model.fit(trainRI, trainRL)
-acc = model.score(testRI, testRL)
-print("[INFO] raw pixel accuracy: {:.2f}%".format(acc * 100))
+	#print(labels)
 
-# train and evaluate a k-NN classifer on the histogram
-# representations
-print("[INFO] evaluating histogram accuracy...")
-model = KNeighborsClassifier(n_neighbors=args["neighbors"],
-                             n_jobs=args["jobs"])
-model.fit(trainFeat, trainLabels)
-acc = model.score(testFeat, testLabels)
-print("[INFO] histogram accuracy: {:.2f}%".format(acc * 100))
+	# show some information on the memory consumed by the raw images
+	# matrix and features matrix
+	rawImages = np.array(rawImages)
+	features = np.array(features)
+	labels = np.array(labels)
+	print("[INFO] pixels matrix: {:.2f}MB".format(
+		rawImages.nbytes / (1024 * 1000.0)))
+	print("[INFO] features matrix: {:.2f}MB".format(
+		features.nbytes / (1024 * 1000.0)))
 
-#model = cPickle.loads(open("model.cpickle", "rb").read()) #to read model
-f = open("model.cpickle", "wb")
-f.write(cPickle.dumps(model))
-f.close()
+	# partition the data into training and testing splits, using 75%
+	# of the data for training and the remaining 25% for testing
+	(trainRI, testRI, trainRL, testRL) = train_test_split(
+		rawImages, labels, test_size=0.25, random_state=42)
+	(trainFeat, testFeat, trainLabels, testLabels) = train_test_split(
+		features, labels, test_size=0.25, random_state=42)
+
+	# train and evaluate a k-NN classifer on the raw pixel intensities
+	print("[INFO] evaluating raw pixel accuracy...")
+	model = KNeighborsClassifier(n_neighbors=args["neighbors"],
+										n_jobs=args["jobs"], metric="cityblock") #manhattan distance
+	model.fit(trainRI, trainRL)
+	acc = model.score(testRI, testRL)
+	print("[INFO] raw pixel accuracy: {:.2f}%".format(acc * 100))
+
+	# train and evaluate a k-NN classifer on the histogram
+	# representations
+	print("[INFO] evaluating histogram accuracy...")
+	model = KNeighborsClassifier(n_neighbors=args["neighbors"],
+										n_jobs=args["jobs"])
+	model.fit(trainFeat, trainLabels)
+	acc = model.score(testFeat, testLabels)
+	print("[INFO] histogram accuracy: {:.2f}%".format(acc * 100))
+
+	f = open("model.cpickle", "wb")
+	f.write(cPickle.dumps(model))
+	f.close()
