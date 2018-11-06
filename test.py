f1f7c1884d2235c817bf2c200935bfedec7052f5
Murilo Baldi <murilo2.baldi@usp.br>
2018-11-06 15:57:27 -0200

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from claptcha import Claptcha
 onlyfiles = [f for f in listdir("dataset-generator/fonts")
              if isfile(join("dataset-generator/fonts", f))]
 
-character = input('Type character :')
+character = input('Type character : ')
 
 c = Claptcha(character, "dataset-generator/fonts/"+onlyfiles[random.randint(0, len(onlyfiles)-1)], (80, 80),
              resample=Image.BICUBIC, noise=0.3, margin=(5, 5))
